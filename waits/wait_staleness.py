1d125c3cb203a870557569ccad31dbb37d08c93f
NataliKucher <40642969+NataliKucher@users.noreply.github.com>
2018-08-02 02:40:51 +0300

    corrected

--- waits/wait_staleness.py
+++ waits/wait_staleness.py
@@ -3,17 +3,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 driver = webdriver.Chrome()
+
 driver.get('https://www.vpnunlimitedapp.com/en/pricing')
-
-driver.find_element_by_xpath("//h4[@class='price_name' and text()='1 year']").click()
+driver.find_element_by_xpath("//a[@class='prices_cnt--item']").click()
 wait = WebDriverWait(driver, 10)
-wait.until(EC.visibility_of_element_located((By.XPATH,"//div[@class='log_change_plan_btn']"))).click()
-
-element = driver.find_element_by_xpath("//h2[text()='Login using an existing KeepSolid ID']")
-wait = WebDriverWait(driver, 10)
-driver.refresh()
-wait.until(EC.staleness_of(element))
+wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='pricing_title_in_header']/descendant::h2")))
+element = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[contains(text(),'Change plan')]")))
+element.click()
+el2 = driver.find_element_by_xpath("//div[@class='pricing_title_in_header']/descendant::h2")
+driver.refresh()  # because el2 still place in the DOM 
+wait.until(EC.staleness_of(el2))
 
 driver.quit()
